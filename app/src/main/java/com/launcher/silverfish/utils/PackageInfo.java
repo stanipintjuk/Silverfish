9853173ef352d23c275e1fbe29f6451de0979be0
Lonami <totufals@hotmail.com>
2016-08-13 12:41:18 +0200

    Added more engines for determining an app's category
    Supported engines: Google Play, F-Droid

--- app/src/main/java/com/launcher/silverfish/utils/PackageInfo.java
+++ app/src/main/java/com/launcher/silverfish/utils/PackageInfo.java
@@ -23,6 +23,9 @@ public class PackageInfo {
 
     public static final String DEFAULT_CATEGORY = "UNKNOWN";
 
+    public static final String ENGINE_PLAY = "play";
+    public static final String ENGINE_FDROID = "f-droid";
+
     //endregion
 
     //region Interfaces
@@ -60,8 +63,9 @@ public class PackageInfo {
         return packages;
     }
 
-    public void getPackagesCategoryAsync(PackageCategoryListener listener, String... packages) {
-        new CategoryFinderTask(listener).execute(packages);
+    // Note that multiple categories will be listed as "CATEGORY1,CATEGORY2"
+    public void getPackagesCategoryAsync(PackageCategoryListener listener, String engine, String... packages) {
+        new CategoryFinderTask(listener, engine).execute(packages);
     }
 
     //endregion
@@ -74,10 +78,12 @@ public class PackageInfo {
 
         // Save the listener so we can notify when we found a category
         PackageCategoryListener listener;
+        String engine; // What engine are we using for finding out the category?
 
         // Constructor
-        public CategoryFinderTask(PackageCategoryListener listener) {
+        public CategoryFinderTask(PackageCategoryListener listener, String engine) {
             this.listener = listener;
+            this.engine = engine;
         }
 
         @Override
@@ -97,11 +103,15 @@ public class PackageInfo {
         // Actual work
 
         // These strings are used to sniff the HTML for finding the app category
-        final static String categorySearchStart = "<a class=\"document-subtitle category\" href=\"/store/apps/category/";
-        final static String categorySearchEnd = "\">";
+        // cs stands for Category Search
+        final static String csPlayStart = "<a class=\"document-subtitle category\" href=\"/store/apps/category/";
+        final static String csPlayEnd = "\">";
 
-        // Append the package to this string to get the right url
-        final static String gplayPackageUrl = "https://play.google.com/store/apps/details?id=";
+        final static String csFDroidStart = "Categories:";
+
+        // Format the package to this string to get the right url
+        final static String playPackageUrl = "https://play.google.com/store/apps/details?id=%s";
+        final static String fdroidPackageUrl = "https://gitlab.com/fdroid/fdroiddata/raw/master/metadata/%s.txt";
 
         // Finds the given package name category
         private String findCategory(String packageName) {
@@ -112,25 +122,37 @@ public class PackageInfo {
             String line;
 
             try {
-                url = getPlayUrl(packageName);
+                url = getPackageUrl(packageName);
                 is = url.openStream();
                 br = new BufferedReader(new InputStreamReader(is));
 
-                // Read the HTML
-                while ((line = br.readLine()) != null) {
+                switch (engine) {
+                    case ENGINE_PLAY:
+                        // Read the HTML
+                        while ((line = br.readLine()) != null) {
 
-                    int idx = line.indexOf(categorySearchStart);
-                    if (idx != -1) { // If we found the category start
+                            int idx = line.indexOf(csPlayStart);
+                            if (idx != -1) { // If we found the category start
 
-                        // Move the index by the needle length to omit it in the substring
-                        idx += categorySearchStart.length();
+                                // Move the index by the needle length to omit it in the substring
+                                idx += csPlayStart.length();
 
-                        // Determine where the category name ends
-                        int endIdx = line.indexOf(categorySearchEnd, idx);
+                                // Determine where the category name ends
+                                int endIdx = line.indexOf(csPlayEnd, idx);
 
-                        // Return category name
-                        return line.substring(idx, endIdx);
-                    }
+                                // Return category name
+                                return line.substring(idx, endIdx);
+                            }
+                        }
+                        break;
+
+                    case ENGINE_FDROID:
+                        // F-Droid has the category at the start of the line, as "Category:App category"
+                        while ((line = br.readLine()) != null) {
+                            if (line.startsWith(csFDroidStart)) {
+                                return line.substring(csFDroidStart.length()).toUpperCase();
+                            }
+                        }
                 }
             } catch (Exception ex) {
                 ex.printStackTrace();
@@ -143,10 +165,20 @@ public class PackageInfo {
             return DEFAULT_CATEGORY;
         }
 
-        // Returns the Google play url for the given package
-        private URL getPlayUrl(String packageName) throws IllegalArgumentException {
+        // Returns the package url for the given package, based on the selected engine
+        private URL getPackageUrl(String packageName) throws Exception {
             try {
-                return new URL(gplayPackageUrl + packageName);
+
+                switch (engine) {
+                    case ENGINE_PLAY:
+                        return new URL(String.format(playPackageUrl, packageName));
+
+                    case ENGINE_FDROID:
+                        return new URL(String.format(fdroidPackageUrl, packageName));
+
+                    default:
+                        throw new Exception("Unknown engine");
+                }
             } catch (MalformedURLException ex) {
                 throw new IllegalArgumentException("Invalid package name");
             }
