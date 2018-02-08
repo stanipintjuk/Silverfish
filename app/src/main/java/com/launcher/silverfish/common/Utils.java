709f558838bb0a96263c1ebd4bbeed0d88ad2517
MPArnold <koparapara@gmail.com>
2018-02-08 21:56:04 +1300

    support 'Drag app to Home page'

--- app/src/main/java/com/launcher/silverfish/common/Utils.java
+++ app/src/main/java/com/launcher/silverfish/common/Utils.java
@@ -26,13 +26,24 @@ import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.view.Display;
+import android.view.DragEvent;
 import android.widget.ImageView;
+
+import java.lang.ref.WeakReference;
 
 /**
  * Created by Stanislav Pintjuk on 8/3/16.
  * E-mail: stanislav.pintjuk@gmail.com
  */
 public class Utils {
+
+    /**
+     * Is supplied drag event within the 'move to home page' zone?
+     */
+    public static boolean isBeyondRightHandThreshold(Activity a, DragEvent dragEvent) {
+        int xThreshold = Utils.getScreenDimensions(a).x - Constants.SCREEN_CORNER_THRESHOLD;
+        return (dragEvent.getX() > xThreshold);
+    }
 
     public static Point getScreenDimensions(Activity activity) {
         // Get the screen size
@@ -47,47 +58,57 @@ public class Utils {
         int screen_width = screensize.x;
         int screen_height = screensize.y;
         // Set the threshold to be 10% of the screen height
-        float thresholdx = 20.0f*screen_height/100.0f;
-        float thresholdy = 10.0f*screen_height/100.0f;
-        return (y >= screen_height - thresholdy && x <= screen_width - thresholdx && x >= 0+thresholdx);
+        float thresholdx = 20.0f * screen_height / 100.0f;
+        float thresholdy = 10.0f * screen_height / 100.0f;
+        return (y >= screen_height - thresholdy && x <= screen_width - thresholdx && x >= 0 + thresholdx);
     }
 
-    public static void loadAppIconAsync(final PackageManager pm, final String appInfo, final ImageView im ){
 
-        // Create an async task
-        AsyncTask<Void,Void,Drawable> loadAppIconTask = new AsyncTask<Void, Void, Drawable>() {
+    // 08Feb2018 Forced to rewrite as static subclass to satisfy Android Studio 3 stringencies!
+    // [Refer https://stackoverflow.com/a/46166223/2376004]
+    /** AsyncTask to render application icon: */
+    private static class loadAppIconTask extends AsyncTask<Void, Void, Drawable> {
+        private Exception exception = null;
+        private WeakReference<PackageManager> pm_wr;
+        private WeakReference<String> appInfo_wr;
+        private WeakReference<ImageView> iv_wr;
 
-            // Keep track of all the exceptions
-            private Exception exception = null;
+        // Constructor
+        loadAppIconTask(PackageManager pm, String appInfo, ImageView iv) {
+            pm_wr = new WeakReference<>(pm);
+            appInfo_wr = new WeakReference<>(appInfo);
+            iv_wr = new WeakReference<>(iv);
+        }
 
-
-            @Override
-            protected Drawable doInBackground(Void... voids) {
-                // load the icon
-                Drawable app_icon = null;
-                try {
-                    app_icon = pm.getApplicationIcon(appInfo);
-
-                } catch (PackageManager.NameNotFoundException e) {
-                    e.printStackTrace();
-                    exception = e;
-                }
-
-                return app_icon;
+        @Override
+        protected Drawable doInBackground(Void... voids) {
+            // load the icon
+            Drawable app_icon = null;
+            try {
+                PackageManager pm = pm_wr.get();
+                String appInfo = appInfo_wr.get();
+                if (pm==null || appInfo==null ) return null;
+                app_icon = pm.getApplicationIcon(appInfo);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+                exception = e;
             }
+            return app_icon;
+        }
 
-            @Override
-            protected void onPostExecute(Drawable app_icon){
-                if (exception == null) {
-                    im.setImageDrawable(app_icon);
-
-                } else {
-                    Log.d("Utils.loadAppIconAsync", "ERROR Could not load app icon.");
-
-                }
+        @Override
+        protected void onPostExecute(Drawable app_icon) {
+            ImageView iv = iv_wr.get();
+            if (exception == null && app_icon != null && iv != null) {
+                iv.setImageDrawable(app_icon);
+                //Log.d("Utils.loadAppIconTask", "Loaded icon for '"+appInfo_wr.get()+"' OK");
+            } else {
+                Log.d("Utils.loadAppIconTask", "ERROR Could not load app icon.");
             }
-        };
+        }
+    }
 
-        loadAppIconTask.execute(null,null,null);
+    public static void loadAppIconAsync(PackageManager pm, String appInfo, ImageView iv ){
+        new loadAppIconTask(pm, appInfo, iv).execute(null,null,null);
     }
 }
